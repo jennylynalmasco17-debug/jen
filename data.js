9fbade98a27e12bf03bd1ea996397f48d5f11832
jennylynalmasco17-debug <jennylynalmasco17@gmail.com>
2026-04-30 15:41:07 +0800

    wip

---
+++ data.js
@@ -0,0 +1,22 @@
+export const products = [
+    {
+        productName: "coquette locket necklace" ,
+        price: 772 ,
+        isAvailable: true ,
+        image: "https://www.etsy.com/listing/1763620657/coquette-locket-necklace?click_key=911b1b18-b726-47df-ac9e-879ec3bc74ba%3ALT82a9fc3101d0e48addea6903f735f416ddc7f575&click_sum=dc579b10&ls=s&ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=coquette+accessories&ref=search_grid-422450-1-1&sr_prefetch=0&pf_from=market&content_source=911b1b18-b726-47df-ac9e-879ec3bc74ba%253ALT82a9fc3101d0e48addea6903f735f416ddc7f575"
+    },
+
+    {
+        productName: "korilakkuma with pink bow Phone Charm" ,
+        price: 257 ,
+        isAvailable: true ,
+        image: "https://www.etsy.com/listing/4471404986/korilakkuma-with-pink-bow-phone-charm?click_key=911b1b18-b726-47df-ac9e-879ec3bc74ba%3ALT84bbee95228f19576cb632e44785edb681116657&click_sum=16c22d48&ls=s&ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=coquette+accessories&ref=search_grid-4413-1-8&sr_prefetch=0&pf_from=market&content_source=911b1b18-b726-47df-ac9e-879ec3bc74ba%253ALT84bbee95228f19576cb632e44785edb681116657"
+    },
+
+    {
+        productName: "Coquette Balletcore Bracelet" ,
+        price: 752 ,
+        isAvailable: true ,
+        image: "https://www.etsy.com/listing/1783944173/coquette-balletcore-bracelet-cluster?click_key=911b1b18-b726-47df-ac9e-879ec3bc74ba%3ALTa4bbd6a2cecd1af1509cbcde94b7af0f945e27e3&click_sum=5cf2858f&ls=s&ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=coquette+accessories&ref=search_grid-4413-1-6&sr_prefetch=0&pf_from=market&content_source=911b1b18-b726-47df-ac9e-879ec3bc74ba%253ALTa4bbd6a2cecd1af1509cbcde94b7af0f945e27e3" ,
+    },
+];
